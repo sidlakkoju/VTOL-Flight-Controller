cce3fad71b50c63ebf1526b4afd815d58d47399e
sidlakkoju <siddharth.lakkoju@gmail.com>
2023-03-10 14:59:01 -0500

    push before branching

--- src/dRehmFlight_Teensy_BETA_1.3.ino
+++ src/dRehmFlight_Teensy_BETA_1.3.ino
@@ -209,9 +209,10 @@ float Kd_yaw = 0.00015;       //Yaw D-gain (be careful when increasing too high,
 //NOTE: Pin 13 is reserved for onboard LED, pins 18 and 19 are reserved for the MPU6050 IMU for default setup
 //Radio:
 //Note: If using SBUS, connect to pin 21 (RX5), if using DSM, connect to pin 15 (RX3)
-const int ch1Pin = 15; //throttle
-const int ch2Pin = 16; //ail
-const int ch3Pin = 17; //ele
+
+const int ch1Pin = 17; //throttle edited
+const int ch2Pin = 15; //ail edited
+const int ch3Pin = 16; //ele edited
 const int ch4Pin = 20; //rudd
 const int ch5Pin = 21; //gear (throttle cut)
 const int ch6Pin = 22; //aux1 (free aux channel)
